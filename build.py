7fa434f3f20a690d699ce43205612232db3076bf
Ross Duggan <rduggan@engineyard.com>
2013-04-01 23:13:53 +0100

    Add plumbing for date. Tweak CSS.

--- build.py
+++ build.py
@@ -2,6 +2,15 @@ import pystache
 import json
 import sys
 import hashlib
+import re
+from datetime import datetime as dt
+
+# Do some datetime fiddling
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
 
 # Grab the first argument raw
 data_location = sys.argv[1]
@@ -15,6 +24,11 @@ css_hash = hashlib.md5(open(css_location,'rb').read()).hexdigest()
 # Pull in the JSON file
 with open(data_location) as f:
    data = f.read()
+
+m = re.search('\d+/\d+/\d+', data_location)
+filename_date = m.group(0)
+time_from_file = dt.strptime(filename_date, "%Y/%m/%d")
+string_from_time = custom_strftime("%B {S}, %Y", time_from_file)
 
 # Convert to object
 entry = json.loads(data)
@@ -34,6 +48,7 @@ generated = pystache.render(template, {
    'verb':entry['verb'],
    'url': entry['url'],
    'title':entry['title'],
+   'time':string_from_time,
    'contributor':contributor,
    'css_hash':css_hash
 })
